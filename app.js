825e6abcb6370a26736cf15c4d38b35350ef720b
Basma Eid <basmanizar@outlook.com>
2020-05-14 22:12:55 +0300

    Problem 5 has solved

--- app.js
+++ app.js
@@ -81,7 +81,6 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-    sum(sumArr[0],sumArr[1],sumArr[2])[0];
     var massage = `2,3,4 was passed in as an array of numbers, and 9 is their sum.`;
     return[sum(sum(sumArr[0],sumArr[1],sumArr[2])[0],sumArr[2])[0],massage];
 }
@@ -104,12 +103,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    var massage = `The numbers 2,3,4 have a product of 24.`;
+    return[multiply(multiply(multArr[0],multArr[1],multArr[2])[0],multArr[2])[0],massage];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testArray
-// testMultiplyArray();
+testMultiplyArray();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -138,6 +138,6 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testDynamicArray
-// testMultiplyAnyArray();
+testMultiplyAnyArray();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
